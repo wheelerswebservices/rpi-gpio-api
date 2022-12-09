251a7b4bfcf159599b9bb85df0a74c951d6ecd3f
Justin Wheeler <justin.wheeler@wheelerswebservices.com>
2022-12-08 22:53:06 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# rpi-gpio-api
+# Raspberry PI GPIO API
+
+## quick-start
+
+![Quick Start 07](quick-start/07.png)
+
+![Quick Start 07](quick-start/08.png)
+
+![Quick Start 07](quick-start/09.png)
+
+> https://www.canakit.com/Media/CanaKit-Raspberry-Pi-Quick-Start-Guide-4.0.pdf
+
+
+## running the app
+
+Install dependencies.
+```
+pip install -r app/requirements.txt
+
+Start the app. The light should start blinking.
+```
+python app/index.py
+```
+
+## testing the app
+
+Is the app listening?
+```
+curl localhost:5000/
+```
+
+Turn the light on and off with a delay.
+```
+curl localhost:5000/blink
+```
+
+Turn the light off.
+```
+curl localhost:5000/off
+```
+
+Turn the light on.
+```
+curl localhost:5000/on
+```
---
+++ app/index.py
@@ -0,0 +1,63 @@
+import RPi.GPIO as GPIO
+import threading
+import time
+
+from flask import Flask, render_template, g
+
+
+class BlinkingThread(threading.Thread):
+    should_blink: bool
+    speed: float
+
+    def __init__(self, should_blink, speed):
+        threading.Thread.__init__(self)
+        self.should_blink = should_blink
+        self.speed = speed
+
+    def run(self):
+        while True:
+            time.sleep(1)
+
+            if self.should_blink:
+              GPIO.output(18, True)
+              time.sleep(self.speed)
+              
+              GPIO.output(18, False)
+              time.sleep(self.speed)
+            
+
+
+app = Flask(__name__)
+
+blinking_thread = BlinkingThread(True, 0.5)
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+
+@app.route("/")
+def success():
+    return render_template('index.html')
+
+@app.route("/blink")
+def blink():
+    blinking_thread.should_blink = True
+    return success()
+
+@app.route("/off")     
+def off():
+    blinking_thread.should_blink = False
+    GPIO.output(18, False)
+    return success()
+
+@app.route("/on")     
+def on():
+    blinking_thread.should_blink = False
+    GPIO.output(18, True)
+    return success()
+
+
+if __name__ == "__main__":
+    blinking_thread.start()
+    app.run()
+    
